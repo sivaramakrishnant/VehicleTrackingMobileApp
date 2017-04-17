b9d52362ff27ec88d4c6d94090337bc9435089d3
sivaramakrishnant <sivaramakrishnan.t@mitosistech.com>
2017-04-17 11:36:00 +0530

    Initial commit

---
+++ app/src/main/java/com/mitosis/vehicletrackingapp/vehicletrackingmobileapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mitosis.vehicletrackingapp.vehicletrackingmobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Shiva has commited
+// on this line
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
